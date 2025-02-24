8176f62156e5e3ed1d37cf1aa456e6dbdbf0428b
test <149031026+p-k-pavan@users.noreply.github.com>
2025-02-24 13:26:57 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,8 +1,8 @@
 def call(String imageName) {
-    echo "Pushing ${imageName} to Docker Hub"
+    echo "Pushing ${imageName}:latest to Docker Hub"
 
-    withCredentials([usernamePassword(credentialsId: 'dockerHubCred', usernameVariable: 'DOCKER_USE', passwordVariable: 'DOCKER_PASS')]) {
-        sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
+    withCredentials([usernamePassword(credentialsId: 'DOCKER_HUB_CREDENTIALS', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+        sh "echo \$DOCKER_PASS | docker login --username \$DOCKER_USER --password-stdin"
         sh "docker tag ${imageName}:latest \$DOCKER_USER/${imageName}:latest"
         sh "docker push \$DOCKER_USER/${imageName}:latest"
     }
